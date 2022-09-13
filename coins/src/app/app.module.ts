70210e6a1164f5ec06003fb52bde505e141e545b
jclois <65991273+jclois@users.noreply.github.com>
2022-09-13 16:02:00 -0400

    1 file modified, 4 added

--- coins/src/app/app.module.ts
+++ coins/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoinsComponent } from './coins/coins.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoinsComponent
   ],
   imports: [
     BrowserModule

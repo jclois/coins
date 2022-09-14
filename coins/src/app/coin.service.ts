b571cbaed929825463db181a90f07a20bb90aed3
jclois <65991273+jclois@users.noreply.github.com>
2022-09-13 20:02:04 -0400

    1 file modified

--- coins/src/app/coin.service.ts
+++ coins/src/app/coin.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Coin } from '../coins';
-import { COINS } from '../mock-coins';
+import { Coin } from './coins';
+import { COINS } from './mock-coins';
 
 @Injectable({
   providedIn: 'root'

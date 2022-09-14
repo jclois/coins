4d4b3c7d8a05edf4ea9c2f5dfe9f560b804aef14
jclois <65991273+jclois@users.noreply.github.com>
2022-09-13 20:55:26 -0400

    1 file modified

--- coins/src/app/app-routing.module.ts
+++ coins/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes =[
   [path: ''], redirectTo: '/dashboard', pathMatch: 'full'},
-  {path: 'coins', component: CoinsComponent}
+  {path: 'coins', component: CoinsComponent},
   {path: 'dashboard', component: DashboardComponent},
 ];
 

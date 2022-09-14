23afa357148ceb5259efc9e308a876ea0c051e3d
jclois <65991273+jclois@users.noreply.github.com>
2022-09-13 20:52:25 -0400

    1 file modified

--- coins/src/app/app-routing.module.ts
+++ coins/src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes} from '@angular/router';
 import { CoinsComponent } from './coins/coins.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes =[
+  [path: ''], redirectTo: '/dashboard', pathMatch: 'full'},
   {path: 'coins', component: CoinsComponent}
+  {path: 'dashboard', component: DashboardComponent},
 ];
 
 @NgModule({

a2124c1018689979cf2c514ffeb9b87a0c6aed2a
jclois <65991273+jclois@users.noreply.github.com>
2022-09-13 20:48:36 -0400

    1 file modified

--- coins/src/app/dashboard/dashboard.component.ts
+++ coins/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Coin } from '../coins';
+import { COINS } from '../mock-coins';
+import { CoinService } from '../coin.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: [ './dashboard.component.css' ]
 })
 export class DashboardComponent implements OnInit {
+  heroes: Coin[] = [];
 
-  constructor() { }
+  constructor(private coinService: CoinService) { }
 
   ngOnInit(): void {
+    this.getCoins();
   }
 
+  getCoins(): void {
+    this.coinService.getCoins().subscribe(coins => this.coins = coins.slice(1, 5));
+  }
 }

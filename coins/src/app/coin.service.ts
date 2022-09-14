3f6ee73b1b9262135e2bddb2b0f7efd1d34dbee1
jclois <65991273+jclois@users.noreply.github.com>
2022-09-13 20:17:21 -0400

    1 file modified

--- coins/src/app/coin.service.ts
+++ coins/src/app/coin.service.ts
@@ -8,7 +8,7 @@ import { Observable, of } from 'rxjs';
 })
 export class CoinService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getCoins(): Observable<Coin[]> {
     return of(COINS);

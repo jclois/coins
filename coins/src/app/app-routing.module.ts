b12692522c60cddfd65e85a442c89242c901d21b
jclois <65991273+jclois@users.noreply.github.com>
2022-09-13 20:40:44 -0400

    1 file modified

--- coins/src/app/app-routing.module.ts
+++ coins/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes} from '@angular/router';
+import { CoinsComponent } from './coins/coins.component';
 
 const routes: Routes =[
   {path: 'coins', component: CoinsComponent}

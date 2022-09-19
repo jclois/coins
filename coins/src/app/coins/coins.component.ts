fb1a336a09185ee03077d2540f9dc8adfe25be7b
jclois <65991273+jclois@users.noreply.github.com>
2022-09-18 21:15:58 -0400

    1 file modified

--- coins/src/app/coins/coins.component.ts
+++ coins/src/app/coins/coins.component.ts
@@ -11,8 +11,8 @@ import { Observable } from 'rxjs';
 })
 export class CoinsComponent implements OnInit {
 
-  //*coins: Coin[];
-  coins: Observable<Coin[]>;
+  coins: Coin[];
+  //*coins: Observable<Coin[]>;
   selectedCoin: Coin;
 
   constructor(private coinService: CoinService) { }

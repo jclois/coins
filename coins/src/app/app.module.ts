45bf314922732dc76c432cd989e15980a5f71a3a
jclois <65991273+jclois@users.noreply.github.com>
2022-10-20 21:10:13 -0400

    1 file modified

--- coins/src/app/app.module.ts
+++ coins/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CoinsComponent } from './coins/coins.component';

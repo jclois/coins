367e1298d9d7ab088988a502b47d06922e8b489e
jclois <65991273+jclois@users.noreply.github.com>
2022-11-11 20:09:21 -0500

    4 files modified

--- coins/src/app/app.module.ts
+++ coins/src/app/app.module.ts
@@ -30,8 +30,8 @@ import { ExtGoldApiComponent } from './ext-gold-api/ext-gold-api.component';
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(
-    InMemoryDataService, { dataEncapsulation: false })
+    //HttpClientInMemoryWebApiModule.forRoot(
+    //InMemoryDataService, { dataEncapsulation: false })
   ],
   providers: [],
   bootstrap: [AppComponent]
